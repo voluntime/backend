6d7c07b28754f8111bd9f9e2acd32f755c9cac35
Trey Moen <trey@moen.ai>
2022-04-01 20:29:08 -0700

    fixed package name

--- src/main/java/me/voluntime/backend/BackendApplication.java
+++ src/main/java/me/voluntime/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package me.vontime.backend;
+package me.voluntime.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/me/voluntime/backend/BackendApplicationTests.java
+++ src/test/java/me/voluntime/backend/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package me.vontime.backend;
+package me.voluntime.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

362653b183f312d89acaf3e1b2527c931f5f1378
Trey Moen <trey@moen.ai>
2022-04-02 10:28:20 -0700

    allow self signed certs in Heroku only BIG YIKES

--- src/db/index.js
+++ src/db/index.js
@@ -3,9 +3,11 @@ const connectionString =
     process.env.DATABASE_URL ||
     "postgres://voluntime:voluntime@localhost:5432/voluntime"
 
+const sslConfig = !!process.env.DATABASE_URL ? { rejectUnauthorized: false} : false;
+
 const dbPool = new Pool({
     connectionString,
-    ssl: !!process.env.DATABASE_URL
+    ssl: sslConfig,
 });
 
 module.exports.pool = dbPool;

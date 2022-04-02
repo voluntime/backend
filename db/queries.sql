c6928751da1063295e8a9755d97ab52fb7532390
Adam Hearn <22334119+hearnadam@users.noreply.github.com>
2022-04-02 11:59:10 -0700

    Update query for post

--- db/queries.sql
+++ db/queries.sql
@@ -10,6 +10,7 @@ SELECT USERNAME, NAME, ZIPCODE, ORGANIZATION, VERIFIED, BIO FROM VOLUNTEER WHERE
 SELECT * FROM FULL_POST WHERE ID = $1;
 
 -- get post via id
--- select * from full_post p full outer join post_like l on l.post = p.id full outer join volunteered v on v.post = p.id and v.volunteer = l.volunteer where v.volunteer = 'adam' and p.id = 1;
+select *, exists(select 1 from full_post join post_like l on id = l.post where id = $1, l.volunteer = $2) as like, exists(select 1 from full_post join volunteered v on id = v.post where id = $1, v.volunteer = $2) as volunteered from full_post p where p.id = $1;
+
 
 -- get via filter
--- db/sampleData.sql
+++ db/sampleData.sql
@@ -1,4 +1,13 @@
-insert into volunteer(username, name, email, zipcode) values('trey_time', 'Trey Moen', 'trey@moen.ai', 97132);
-insert into volunteer(username, name, email, zipcode) values('adam', 'Adam Hearn', 'ahearn19@georgefox.edu', 97132);
-insert into volunteer(username, name, email, zipcode) values('blake', 'Blake Bryan', 'bbryan19@georgefox.edu', 97132);
+insert into volunteer(username, name, email, password, zipcode) values('trey_time', 'Trey Moen', 'trey@moen.ai', 'poop', 97132);
+insert into volunteer(username, name, email, password, zipcode) values('adam', 'Adam Hearn', 'ahearn19@georgefox.edu', 'poop', 97132);
+insert into volunteer(username, name, email, password, zipcode) values('blake', 'Blake Bryan', 'bbryan19@georgefox.edu', 'poop', 97132);
+
 insert into post(created, begins, ends, organizer, title, body, event_location, event_type, goal) values (now(), now(), now(), 'trey_time', 'Help Adam Graduate', 'Please please please', 'Newberg, OR', 'Service', 1);
+
+insert into post_like(post, volunteer) values(1, 'adam');
+insert into volunteered(post, volunteer) values(1, 'adam');
+
+
+select *, exists(select 1 from full_post join post_like l on id = l.post where id = 1 and l.volunteer = 'adam') as like, exists(select 1 from full_post join volunteered v on id = v.post where id = 1 and v.volunteer = 'adam') as volunteered from full_post p where p.id = 1;
+
+

26f91e0a0f14dd2c115ec8a40db8d34ca571ac65
Trey Moen <trey@moen.ai>
2022-04-01 22:36:35 -0700

    add test user route

--- app.js
+++ app.js
@@ -4,10 +4,17 @@ const port = process.env.PORT || 8080;
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/v1", (req, res) => {
     res.send({
         "version": 1
     });
+});
+
+app.get("/v1/user", (req, res) => {
+    res.send({
+        "username": "adam_hearn",
+        "full_name": "Adam Hearn",
+    })
 });
 
 app.listen(port, () => {

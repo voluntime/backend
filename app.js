c78f2cc64dbef70073c8b5d68115abe2693b2de2
Trey Moen <trey@moen.ai>
2022-04-02 16:13:50 -0700

    fix CORS policy for local and remote dev

--- app.js
+++ app.js
@@ -17,7 +17,11 @@ const initSql = fs.readFileSync("./db/schema.sql").toString();
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000", "https://api.volunti.me"],
+    credentials: true
+}));
+
 app.use(express.json());
 app.use(session({
     secret: "voluntime",

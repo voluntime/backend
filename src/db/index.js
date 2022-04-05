d53e705273c9d8a78197266d150bceece46976a4
Trey Moen <trey@moen.ai>
2022-04-05 13:38:59 -0700

    make ssl in prod optional for docker

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,8 @@ const connectionString =
     process.env.DATABASE_URL ||
     "postgres://voluntime:voluntime@localhost:5432/voluntime"
 
-const sslConfig = !!process.env.DATABASE_URL ? { rejectUnauthorized: false} : false;
+const sslConfig = process.env.POSTGRES_SSL &&
+                  process.env.POSTGRES_SSL === "true" ? { rejectUnauthorized: false } : false;
 
 const dbPool = new Pool({
     connectionString,

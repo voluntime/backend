8ec817ffbc91109119914314b0da004183a118f6
Adam Hearn <22334119+hearnadam@users.noreply.github.com>
2022-04-14 15:02:46 -0700

    Migrate: reputation into user endpoint.
    This commit completely eliminates the need for the reputation endpoint.
    In addition, this migrates the hand count calculation to the backend.
    Business logic == backend.

--- src/reputation/index.js
+++
@@ -1,3 +0,0 @@
-const queries = require("./queries");
-
-module.exports = queries;
--- src/user/queries.js
+++ src/user/queries.js
@@ -20,7 +20,8 @@ module.exports.getAllUsers = (req, res, next) => {
 module.exports.getUser = (req, res, next) => {
     const username = req.params.username || "";
 
-    pool.query("SELECT * FROM VOLUNTEER WHERE USERNAME = $1", [username], (err, result) => {
+    pool.query("SELECT V.*, COUNT(VD.FUFILLED) AS REPUTATION FROM VOLUNTEER V JOIN VOLUNTEERED VD ON V.USERNAME = VD.VOLUNTEER WHERE USERNAME = $1 GROUP BY V.USERNAME", [username], (err, result) => {
+
         if (err) return next(err);
 
         if (result.rows.length === 0) {
@@ -32,6 +33,15 @@ module.exports.getUser = (req, res, next) => {
         const user = result.rows[0];
 
         delete user.password;
+
+        // set reputation to be in range of 0-3
+        if (user.reputation > 1 && user.reputation < 5) {
+            user.reputation = 1;
+        } else if (user.reputation > 5 && user.reputation < 10) {
+            user.reputation = 2;
+        } else if (user.reputation >= 10) {
+            user.reputation = 3;
+        }
 
         if (user.username !== req.session.user.username) {
             delete user.email;
